1275bb001750727b09d9f98192541b74c0ec506b
Jo Van Eyck <jo.vaneyck@ae.be>
2020-11-17 09:36:26 +0100

    F# 5

---
+++ day01/part1.fsx
@@ -0,0 +1,9 @@
+﻿#r @"nuget: Unquote"
+open Swensen.Unquote
+
+let path = $@"{__SOURCE_DIRECTORY__}\input.txt"
+let input = System.IO.File.ReadAllLines path
+
+printf "Testing..."
+test <@ 1 + 1 = 4 @>
+printfn "done!"
