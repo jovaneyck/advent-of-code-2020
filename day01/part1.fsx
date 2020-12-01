937b8c0e3afb36e421376ef902ce24d70b1950af
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-01 08:54:37 +0100

    day 1 screencast code

--- day01/part1.fsx
+++ day01/part1.fsx
@@ -6,18 +6,26 @@ let input = System.IO.File.ReadAllLines path |> Seq.map int
 
 let rec combinations list =
     match list with
-    |x :: xs -> 
-        let withX = xs |> List.map (fun y -> (x,y))
-        let others = combinations xs
-        List.append withX others
-    | _ -> []
+    | [] -> []
+    | x :: xs ->
+        let firstPairs = xs |> List.map (fun y -> (x,y))
+        firstPairs @ (combinations xs)
 
-let part1 =
-    input
-    |> Seq.toList
-    |> combinations
-    |> List.find (fun (x,y) -> x + y = 2020)
-    |> (fun (x,y) -> x * y)
+let rec combinations3 list =
+    match list with
+    | [] -> []
+    | x :: xs ->
+        let combos = combinations xs
+        let firstPairs = combos |> List.map (fun (y,z) -> (x,y,z))
+        firstPairs @ (combinations3 xs)
+
+input 
+|> Seq.toList 
+|> combinations3
+|> List.find (fun (x,y,z) -> x + y + z = 2020)
+|> (fun (x,y,z) -> x * y * z)
+
 printf "Testing..."
 test <@ combinations [1;2;3;4] = [(1, 2); (1, 3); (1, 4); (2, 3); (2, 4); (3, 4)] @>
+test <@ combinations3 [1;2;3;4] = [(1, 2, 3); (1, 2, 4); (1, 3, 4); (2, 3, 4)]  @>
 printfn "done!"
