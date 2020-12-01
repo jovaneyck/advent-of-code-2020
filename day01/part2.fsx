1b4acd37d8e2b75992477df124dcb5c231415756
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-01 06:42:13 +0100

    #1 tail recursion

--- day01/part2.fsx
+++ day01/part2.fsx
@@ -5,21 +5,23 @@ let path = $@"{__SOURCE_DIRECTORY__}\input.txt"
 let input = System.IO.File.ReadAllLines path |> Seq.map int
 
 let rec combinations list =
-    match list with
-    |x :: xs -> 
-        let withX = xs |> List.map (fun y -> (x,y))
-        let others = combinations xs
-        List.append withX others
-    | _ -> []
+    let rec combinations acc list =
+        match list with
+        |x :: xs -> 
+            let withX = xs |> List.map (fun y -> (x,y))
+            combinations (withX :: acc) xs
+        | _ -> acc |> List.concat
+    combinations [] list
 
-let rec combinations3 list =
-    match list with
-    |x :: xs -> 
-        let tuples = combinations xs
-        let withX = tuples |> List.map (fun (y,z) -> (x,y,z))
-        let others = combinations3 xs
-        List.append withX others
-    | _ -> []
+let combinations3 list =
+    let rec combinations3 acc list =
+        match list with
+        |x :: xs -> 
+            let tuples = combinations xs
+            let withX = tuples |> List.map (fun (y,z) -> (x,y,z))
+            combinations3 (withX :: acc) xs
+        | _ -> acc |> List.concat
+    combinations3 [] list
 
 let part2 =
     input
@@ -29,6 +31,6 @@ let part2 =
     |> (fun (x,y,z) -> x * y * z)
 
 printf "Testing..."
-test <@ combinations [1;2;3;4] = [(1, 2); (1, 3); (1, 4); (2, 3); (2, 4); (3, 4)] @>
-test <@ combinations3 [1;2;3;4;5] = [(1, 2, 3); (1, 2, 4); (1, 2, 5); (1, 3, 4); (1, 3, 5); (1, 4, 5); (2, 3, 4);(2, 3, 5); (2, 4, 5); (3, 4, 5)] @>
+test <@ combinations [1;2;3;4] = [(3, 4); (2, 3); (2, 4); (1, 2); (1, 3); (1, 4)] @>
+test <@ combinations3 [1;2;3;4] = [(2, 3, 4); (1, 3, 4); (1, 2, 3); (1, 2, 4)] @>
 printfn "done!"
