3c5a29d717f07686fe02aed4d00cf69b3f636c6e
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-04 07:47:17 +0100

    switched day 1 and 2

--- day04/part1.fsx
+++ day04/part1.fsx
@@ -24,6 +24,7 @@ type Passport = KVP list
 type PassportRegex = Regex<"(?<key>\w*):(?<value>\S*)">
 let regex = PassportRegex()
 
+
 let parsePassport text : Passport = 
     regex.TypedMatches(text)
         |> Seq.map (fun kvp -> {Key = kvp.key.Value; Value = kvp.value.Value})
@@ -34,57 +35,9 @@ let parse (text : string) =
     |> Seq.map parsePassport
 
 let requiredKeys = ["byr"; "iyr"; "eyr"; "hgt"; "hcl"; "ecl"; "pid"]
-
-let hasRequiredKeys (passport : Passport) = 
+let isValid (passport : Passport) = 
     requiredKeys
     |> Seq.forall (fun k -> passport |> List.exists (fun kvp -> kvp.Key = k))
-
-let yearBetween min max (kvp : KVP) =
-    let year = int kvp.Value
-    kvp.Value.Length = 4 && min <= year && year <= max
-
-type HeightRegex = Regex<"^(?<value>\d*)(?<unit>(cm|in))$">
-let heightRegex = HeightRegex()
-let validHeight (h : string) = 
-    match heightRegex.TryTypedMatch(h) with
-    | None -> false
-    | Some m ->
-        let h = int m.value.Value
-        match m.unit.Value with
-        | "cm" -> 150 <= h && h <= 193
-        | "in" -> 59 <= h && h <= 76
-
-type HairColor = Regex<"^#([0-9]|[a-f]){6}$">
-let hairColorRegex = HairColor()
-let validHairColor (text : string) =
-    match hairColorRegex.TryTypedMatch(text) with
-    | Some _ -> true
-    | _ -> false
-
-let validEyeColor text =
-    ["amb"; "blu"; "brn"; "gry"; "grn"; "hzl"; "oth"]
-    |> List.exists ((=) text)
-
-type PassportId = Regex<"^[0-9]{9}$">
-let passportIdRegex = PassportId()
-let validPassportId text = 
-    match passportIdRegex.TryTypedMatch text with
-    | Some _ -> true
-    | _ -> false
-
-let validKVP (kvp : KVP) = 
-    match kvp.Key with
-    | "byr" -> yearBetween 1902 2002 kvp
-    | "iyr" -> yearBetween 2010 2020 kvp
-    | "eyr" -> yearBetween 2020 2030 kvp
-    | "hgt" -> validHeight kvp.Value
-    | "hcl" -> validHairColor kvp.Value
-    | "ecl" -> validEyeColor kvp.Value
-    | "pid" -> validPassportId kvp.Value
-    | "cid" -> true
-
-let isValid (passport : Passport) = 
-    hasRequiredKeys passport && passport |> List.forall validKVP
 
 let passports = parse input
 Seq.length passports
