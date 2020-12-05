50985793c1e3f8e90aa039f2f27bf2e032fe697f
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-05 15:01:56 +0100

    day 5 even shorter thanks to Xaviers python-fu example :)

--- day05/codegolf.fsx
+++ day05/codegolf.fsx
@@ -1,5 +1,4 @@
 open System
-let input = IO.File.ReadAllLines $@"{__SOURCE_DIRECTORY__}\input.txt"
-  
-let ids = set (Seq.map (Seq.map (function | 'F' | 'L' -> "0" | 'B' | 'R' -> "1" ) >> String.concat "" >> (fun n -> Convert.ToInt32(n, 2))) input)
-Set.difference (set [ ids |> Seq.min .. ids |> Seq.max ]) ids
+let input = IO.File.ReadLines $"{__SOURCE_DIRECTORY__}\input.txt"
+let ids = set(Seq.map (Seq.map (function |'F'|'L'->"0" |'B'|'R'->"1") >> String.concat "" >> (fun n -> Convert.ToInt32(n, 2))) input)
+set [ ids |> Seq.min .. ids |> Seq.max ] - ids
