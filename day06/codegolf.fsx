47f814083235bf26e6a80a90a50b09cf250eccf7
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-06 07:04:24 +0100

    day 6 golfed

---
+++ day06/codegolf.fsx
@@ -0,0 +1,10 @@
+﻿open System
+let input = IO.File.ReadAllText $@"{__SOURCE_DIRECTORY__}\input.txt"
+let none = StringSplitOptions.None
+let groups = input.Split([|"\r\n\r\n"|], none)
+groups 
+|> Seq.map (fun g -> 
+    g.Split([|"\r\n"|], none) 
+    |> Seq.map Set.ofSeq 
+    |> Set.intersectMany ) 
+|> Seq.sumBy Seq.length
--- day06/part1.fsx
+++ day06/part1.fsx
@@ -3,7 +3,7 @@ open Swensen.Unquote
 
 let input = System.IO.File.ReadAllText $@"{__SOURCE_DIRECTORY__}\input.txt"
 let groups = input.Split([|"\r\n\r\n"|], System.StringSplitOptions.RemoveEmptyEntries)
-groups |> Seq.map (fun g -> g |> Seq.distinct |> Seq.except ['\r'; '\n'] |> Seq.length) |> Seq.sum
+groups |> Seq.map (fun g -> g |> Seq.distinct |> Seq.except ['\r'; '\n']) |> Seq.sumBy Seq.length
 
 printf "Test.."
 
