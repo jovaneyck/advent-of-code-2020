3a3f52099bb728501407891e3a9ff9f50213691d
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-10 18:02:00 +0100

    all variants for 10.2

---
+++ day10/part2-bruteforce.fsx
@@ -0,0 +1,33 @@
+﻿#r @"nuget: Unquote"
+open Swensen.Unquote
+
+let input = System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt" |> Seq.map int |> Seq.toList
+let example = [16;10;15;5;1;11;7;19;6;12;4]
+
+let candidates joltage adapters =
+    let rec candidates' acc joltage adapters =
+        match adapters with
+        | [] -> acc
+        | a :: aas when a - joltage <= 3 ->  candidates' ((a, aas) ::acc) joltage aas
+        | _ -> acc
+    candidates' [] joltage adapters |> List.rev
+
+let rec calculateNbPaths joltage adapters = 
+    match adapters with
+    | [_] -> 1L
+    | adapters ->
+        let cands = candidates joltage adapters 
+        cands |> Seq.map (fun (a, aas) -> calculateNbPaths a aas) |> Seq.sum
+
+let solve adapters =
+    let sorted = adapters |> List.sort
+    let deviceAdapter = (sorted |> Seq.last) + 3
+    let allAdapters = sorted @ [deviceAdapter]
+    
+    calculateNbPaths 0 allAdapters
+
+printf "Test.."
+test <@ candidates 4 [5;6;7;10] = [(5, [6; 7; 10]); (6, [7; 10]); (7, [10])] @>
+test <@ solve example = 8L @> 
+//test <@ solve input = 99214346656768L @> //This would run for a very, very, VERY long time :)
+printfn "done!"
---
+++ day10/part2-mutabledict.fsx
@@ -0,0 +1,42 @@
+﻿#r @"nuget: Unquote"
+open Swensen.Unquote
+open System.Collections.Generic
+
+let input = System.IO.File.ReadAllLines $"{__SOURCE_DIRECTORY__}\input.txt" |> Seq.map int |> Seq.toList
+let example = [16;10;15;5;1;11;7;19;6;12;4]
+
+let candidates joltage adapters =
+    let rec candidates' acc joltage adapters =
+        match adapters with
+        | [] -> acc
+        | a :: aas when a - joltage <= 3 ->  candidates' ((a, aas) ::acc) joltage aas
+        | _ -> acc
+    candidates' [] joltage adapters |> List.rev
+
+let rec calculateNbPaths (memo : Dictionary<int,int64>) joltage adapters = 
+    match memo.TryGetValue joltage with
+    | (true, v) -> v
+    | _ ->
+        match adapters with
+        | [_] -> 
+            memo.Add(joltage, 1L)
+            1L
+        | adapters ->
+            let cands = candidates joltage adapters 
+            let sum = cands |> Seq.map (fun (a, aas) -> calculateNbPaths memo a aas) |> Seq.sum
+            memo.Add(joltage, sum)
+            sum
+
+let solve adapters =
+    let sorted = adapters |> List.sort
+    let deviceAdapter = (sorted |> Seq.last) + 3
+    let allAdapters = sorted @ [deviceAdapter]
+    let memo = Dictionary<int,int64> ()
+    
+    calculateNbPaths memo 0 allAdapters
+
+printf "Test.."
+test <@ candidates 4 [5;6;7;10] = [(5, [6; 7; 10]); (6, [7; 10]); (7, [10])] @>
+test <@ solve example = 8L @> 
+test <@ solve input = 99214346656768L @> 
+printfn "done!"
