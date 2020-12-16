33b3a2402a3daba7f9a4ab0a4ad64c20e37ec8aa
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-16 04:53:49 +0100

    clean up 15 using unfold

--- day15/part2.fsx
+++ day15/part2.fsx
@@ -9,6 +9,7 @@ let input =
 let example = [0L;3L;6L]
 
 type Turn = { timestamp : int64; number : int64; lookup : Map<int64, int64> (*nb->last time we heard nb*) }
+
 let initialTurn seed =
     let lookup = seed |> Seq.mapi (fun idx nb -> (nb, int64 idx)) |> Map.ofSeq
     { timestamp = (seed |> Seq.length |> int64) - 1L; number = (seed |> Seq.last); lookup = lookup}
@@ -36,23 +37,26 @@ let next turn =
 let rec generateSeq turn : Turn seq =
     seq {
         yield turn
-        if turn.timestamp % 300_000L = 0L then printfn $"{turn.timestamp}"
+        //if turn.timestamp % 300_000L = 0L then printfn $"{turn.timestamp}"
         yield! generateSeq (next turn)
     }
 
 
 let solve input n =
     let init = initialTurn input
-    let nth = generateSeq init |> Seq.map (fun t -> t.number) |> Seq.item (n - (input |> Seq.length))
+    let sequence = init |> Seq.unfold (fun s -> Some (s.number, next s))
+    let nth = sequence |> Seq.item (n - (input |> Seq.length))
     nth
     
-let part1 = solve input 2020 //2020: 1238L
-#time "on" 
-let part2 = solve input 30_000_000
+//let part1 = solve input 2020 //2020: 1238L
+//#time "on" 
+//let part2 = solve input 30_000_000
 (*
     Real: 00:01:24.886, CPU: 00:01:24.375, GC gen0: 5599, gen1: 857, gen2: 93
     val part2 : int64 = 3745954L
 *)
 
 printf "Test"
+test <@ solve [0L;3L;6L] 2020 = 436L @>
+test <@ solve input 2020 = 1238L  @>
 printfn "done!"
