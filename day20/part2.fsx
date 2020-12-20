ca7aa06c86d3b28029c08500df7fdad7afb202d2
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-20 13:40:56 +0100

    ready for part2, let's find some sea monsters!

--- day20/part2.fsx
+++ day20/part2.fsx
@@ -1,1 +1,91 @@
-﻿
+﻿#r "nuget: Unquote"
+open Swensen.Unquote
+open System.Text.RegularExpressions
+
+let example = System.IO.File.ReadAllText $"{__SOURCE_DIRECTORY__}\example.txt"  
+let input = System.IO.File.ReadAllText $"{__SOURCE_DIRECTORY__}\input.txt"  
+
+type Tile = { ID : int; contents : char[,] }
+type TileConfigurations = { ID : int; configurations : char[,] list }
+
+let m = array2D [[1;2];[3;4]]
+
+let rot90 m =
+    [for col in (Array2D.length1 m) - 1 .. -1 .. 0 -> 
+        m.[0..,col]
+    ]
+    |> array2D
+
+let flipHorizontal m =
+    [for row in (Array2D.length1 m) - 1 .. -1 .. 0 -> 
+        m.[row,0..]
+    ]
+    |> array2D  
+
+let flipVertical m =
+    [for row in 0 .. (Array2D.length1 m) - 1-> 
+        m.[row,0..] |> Array.rev
+    ]
+    |> array2D
+
+let parse (input : string) = 
+    let tileIDRegex = Regex("Tile (\d*):")
+    let parseTile (text :string) =
+        let lines = text.Split([|"\r\n"|], System.StringSplitOptions.RemoveEmptyEntries)
+        let id = (tileIDRegex.Match(lines.[0])).Groups.[1].Value |> int
+        let contents = array2D lines.[1..]
+        { ID = id; contents = contents }
+
+    input.Split([|"\r\n\r\n"|], System.StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map parseTile
+    |> Seq.toList
+
+let borders (tile : 'a [,]) =
+    [ 
+        tile.[0,0..]
+        tile.[(Array2D.length1 tile - 1),0..]
+        tile.[0..,0]
+        tile.[0..,(Array2D.length1 tile - 1)]
+    ]
+
+let configurations tile = 
+    [tile
+     tile |> rot90
+     tile |> rot90 |> rot90
+     tile |> rot90 |> rot90 |> rot90]
+    |> List.collect (fun t -> [t; flipHorizontal t])
+
+let findCorners configs tiles =
+    let nbUnmatchedBorders tile (configs : TileConfigurations list) = 
+        let bords = borders tile.contents
+        let nbMatchesPerBorder =
+            bords
+            |> Seq.map (fun border -> 
+                configs 
+                |> Seq.filter (fun config -> 
+                    config.configurations 
+                    |> List.exists (fun c -> c |> borders |> List.contains border)) 
+                |> Seq.length)
+        nbMatchesPerBorder |> Seq.filter ((=) 0) |> Seq.length
+    
+    tiles
+    |> List.filter (fun t -> nbUnmatchedBorders t (configs |> List.filter (fun c ->  c.ID <> t.ID)) = 2)
+
+let part1 input =
+    let tiles = parse input
+    let configs = tiles |> List.map (fun t -> { ID = t.ID; configurations = configurations t.contents})
+    let corners = findCorners configs tiles
+
+    corners |> Seq.map (fun t -> int64 t.ID) |> Seq.reduce (*)
+
+#time "on"
+printf "Test.."
+test <@ array2D [[1;2];[3;4]] |> rot90 = array2D [[|2; 4|]; [|1; 3|]]  @>
+test <@ array2D [[1;2];[3;4]] |> rot90 |> rot90 = array2D [[|4; 3|]; [|2; 1|]]  @>
+test <@ array2D [[1;2];[3;4]] |> rot90 |> rot90 |> rot90 |> rot90 = array2D [[1;2];[3;4]]  @>
+test <@ array2D [[1;2];[3;4]] |> flipHorizontal = array2D [[|3; 4|]; [|1; 2|]]  @>
+test <@ array2D [[1;2];[3;4]] |> flipVertical = array2D [[|2; 1|]; [|4; 3|]]  @>
+test <@ array2D [[1;2];[3;4]] |> borders =[[|1; 2|]; [|3; 4|]; [|1; 3|]; [|2; 4|]]  @>
+test <@ part1 example = 20899048083289L @>
+test <@ part1 input = 14129524957217L @>
+printfn "..done!"
