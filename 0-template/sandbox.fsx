e2fcb197fb4e1623fc07d1e5a7621cd4d17818ba
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-22 16:14:30 +0100

    reworked 22 so it now runs in 15 seconds

--- 0-template/sandbox.fsx
+++ 0-template/sandbox.fsx
@@ -18,5 +18,6 @@ let combinationsComprehensions list =
 test <@ combinationsRecursive [1..4] = [(1, 2); (1, 3); (1, 4); (2, 3); (2, 4); (3, 4)]  @>
 test <@ combinationsRecursive [1..4] = combinationsComprehensions [1..4] @>
 
+
 test <@ combinationsRecursive [1;2;1;4] = [(1, 2); (1, 1); (1, 4); (2, 1); (2, 4); (1, 4)]  @>
 test <@ combinationsRecursive [1;2;1;4] = combinationsComprehensions [1;2;1;4] @>
--- day22/part2.fsx
+++ day22/part2.fsx
@@ -25,7 +25,7 @@ let rec playRound (p1, p2) : GameState =
         if gameNb % 1_000L = 0L then printfn "starting sub-game %d" gameNb
         let subDeck1 = p1Deck |> List.take p1Card
         let subDeck2 = p2Deck |> List.take p2Card
-        let winner = playGame [] ({ p1 with cards = subDeck1}, { p2 with cards = subDeck2})
+        let winner = playGame Set.empty ({ p1 with cards = subDeck1}, { p2 with cards = subDeck2})
         match winner.player with
         | 1 -> { p1 with cards = p1Deck @ [p1Card;p2Card] }, { p2 with cards = p2Deck }
         | _ -> { p1 with cards = p1Deck }, { p2 with cards = p2Deck @ [p2Card;p1Card] }
@@ -39,9 +39,9 @@ and playGame history ((p1, p2) as state) : Deck =
     | _, [] -> p1
     | _ -> 
         let next = state |> playRound 
-        if history |> Seq.contains next
+        if history |> Set.contains next
         then p1
-        else playGame (next :: history) next
+        else playGame (history |> Set.add next) next
 
 let score deck =
     deck 
@@ -52,7 +52,17 @@ let score deck =
 
 let parsed = parse input
 #time "on"
-let winner = playGame [] parsed //Real: 00:47:48.010, CPU: 00:46:27.718, GC gen0: 507418, gen1: 397, gen2: 29
+let winner = playGame Set.empty parsed 
+
+//The data structure I was calling for was Queue, but that turned out to be a minor improvement
+//The real win was in using a different structure to keep track of history:
+//Immutable sets based on binary trees, where elements are ordered by F# generic comparison. By default comparison is the F# structural comparison
+//=> O(logn) lookups/inserts!
+
+//with history LIST: Real: 00:47:48.010, CPU: 00:46:27.718, GC gen0: 507418, gen1: 397, gen2: 29
+//with history SET: Real: 00:00:11.265, CPU: 00:00:10.640, GC gen0: 1664, gen1: 82, gen2: 3
+//https://github.com/ralphhendriks/aoc2020/blob/main/22/Program.fs
+
 let part2 = score winner
 
 printf "Test.."
