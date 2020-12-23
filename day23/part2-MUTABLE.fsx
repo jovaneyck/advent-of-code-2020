7ab14f731a8c382ccda0bce71256b8982b1e9233
Van Eyck Jo <jo.vaneyck@gmail.com>
2020-12-23 08:17:22 +0100

    23 cleanup

--- day23/part2-MUTABLE.fsx
+++ day23/part2-MUTABLE.fsx
@@ -6,19 +6,9 @@ let parsed = (input |> Seq.map (string>>int) |> Seq.toList) @ [10..1_000_000]
 
 type LinkedList = { value : int; mutable next : LinkedList option }
 
-let take n node =
-    let rec take acc n node =
-        if n = 0 then acc |> List.rev
-        else take (node.value :: acc) (n-1) (node.next.Value)
-    take [] n node
-
-let rec find n node =
-    if node.value = n then node
-    else find n (node.next.Value)
-
 let toLinkedList list =
     let nodes = list |> List.map (fun n -> { value = n; next = None})
-    //Lets's link
+    
     nodes 
     |> List.pairwise
     |> List.iter (fun (f,s) -> f.next <- Some s)
@@ -37,8 +27,7 @@ let wrap max n =
     else 1 + (n - 1) % max
 
 let rec findDestination maximum current selected =
-    //printfn "figuring out destination given %d %d %A" maximum current selected
-    if selected |> Seq.contains current
+    if selected |> List.contains current
     then findDestination maximum ((current - 1) |> wrap maximum) selected
     else current
 
@@ -54,10 +43,11 @@ let move n (current : LinkedList) =
             let two = one.next.Value
             let three = two.next.Value
             let rest = three.next.Value
-            let destination = findDestination maximum ((current.value - 1) |> wrap maximum) [one.value;two.value;three.value]
-            //printfn "cups: %A" (take 9 current)
-            //printfn "pick up: %d %d %d" one.value two.value three.value
-            //printfn "destination: %A" destination
+            let destination = 
+                findDestination 
+                    maximum 
+                    ((current.value - 1) |> wrap maximum) 
+                    [one.value;two.value;three.value]
 
             let destNode = valuesToNodes |> Map.find destination
             current.next <- Some rest
@@ -70,5 +60,5 @@ let move n (current : LinkedList) =
 #time "on"
 let final = move 10_000_000 start
 
-let one = find 1 final
+let one = valuesToNodes |> Map.find 1
 let part2 = (int64 one.next.Value.value) * (int64 one.next.Value.next.Value.value)
